deb1c1b70406c23975cee20ebf8a1ee1f645ac51
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-02-10 11:19:56 -0300

    feat(views): create registerPage view

--- base/views.py
+++ base/views.py
@@ -40,6 +40,10 @@ def logoutUser(request):
     logout(request)
     return redirect('home')
 
+def registerPage(request):
+    page = 'register'
+    return render(request, 'base/login_register.html')
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
